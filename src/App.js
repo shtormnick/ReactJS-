a439655b639719b794229703581af7332aef5037
lorix <foxred324@gmail.com>
2020-04-22 12:34:03 +0300

    add loader

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, {useEffect} from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
 import AddTodo from './Todo/AddTodo'
-
+import Loader from './Loader'
 
 function App() {
   const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
@@ -13,6 +14,7 @@ function App() {
       .then(todos => {
         setTimeout(() =>{
           setTodos(todos)
+          setLoading(false)
         }, 2000)
       })
   }, [])
@@ -51,9 +53,12 @@ function App() {
       <div className="wrapper">
         <h1>React Tutorial</h1>
         <AddTodo  onCreate={addTodo} />
-        {todos.length ? 
-          <TodoList todos={todos} onToggle={toggleTodo} /> : <p>No todos!</p>
-        }
+        {loading && <Loader/>}
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} /> 
+        ) : loading ? null : (
+          <p>No todos!</p>
+        )}
       </div>
     </Context.Provider>
   );
